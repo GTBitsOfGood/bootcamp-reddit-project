9c678cce4546ac6f4aeade3ee7b3646db58c9b8b
Anand Krishnan <anand.krrishnan@gmail.com>
2020-04-01 21:57:52 -0400

    Added part 1 and part 2 functionality

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,7 +1,98 @@
 const { Post } = require('../models')
 
 module.exports.index = (req, res, next) => {
-  Post.find()
+  let dateRange = req.body.dateRange;
+  if (dateRange != undefined) {
+    let currDate = new Date(req.body.currDate);
+    let date = new Date(req.body.currDate);
+
+    if (dateRange === "Past week") {
+      date.setDate(currDate.getDate() - 7);
+
+      Post.find({"createdAt": {"$gte": date, "$lt": currDate}})
+      .populate('comments')
+      .sort('-createdAt')
+      .then(posts => {
+        res.locals.data = { posts }
+        res.locals.status = 200
+        return next()
+      })
+      .catch(err => {
+        console.error(err)
+        res.locals.error = { error: err.message }
+        return next()
+      })
+
+    } else if (dateRange === "Past month") {
+      date.setDate(currDate.getDate() - 31);
+
+      Post.find({"createdAt": {"$gte": date, "$lt": currDate}})
+      .populate('comments')
+      .sort('-createdAt')
+      .then(posts => {
+        res.locals.data = { posts }
+        res.locals.status = 200
+        return next()
+      })
+      .catch(err => {
+        console.error(err)
+        res.locals.error = { error: err.message }
+        return next()
+      })
+
+    } else if (dateRange === "Past year") {
+      date.setDate(currDate.getDate() - 365);
+
+      Post.find({"createdAt": {"$gte": date, "$lt": currDate}})
+      .populate('comments')
+      .sort('-createdAt')
+      .then(posts => {
+        res.locals.data = { posts }
+        res.locals.status = 200
+        return next()
+      })
+      .catch(err => {
+        console.error(err)
+        res.locals.error = { error: err.message }
+        return next()
+      })
+
+    } else if (dateRange === "A year ago") {
+      date.setDate(currDate.getDate() - 365);
+
+      Post.find({"createdAt": {"$gte": date, "$lt": currDate}})
+      .populate('comments')
+      .sort('-createdAt')
+      .then(posts => {
+        res.locals.data = { posts }
+        res.locals.status = 200
+        return next()
+      })
+      .catch(err => {
+        console.error(err)
+        res.locals.error = { error: err.message }
+        return next()
+      })
+
+    } else { //ancient times
+      date.setDate(currDate.getDate() - 3650);
+
+      Post.find({"createdAt": {"$gte": date, "$lt": currDate}})
+      .populate('comments')
+      .sort('-createdAt')
+      .then(posts => {
+        res.locals.data = { posts }
+        res.locals.status = 200
+        return next()
+      })
+      .catch(err => {
+        console.error(err)
+        res.locals.error = { error: err.message }
+        return next()
+      })
+    }
+  } else { //if the given date range is undefined, do old stuff
+    Post.find()
     .populate('comments')
     .sort('-createdAt')
     .then(posts => {
@@ -14,6 +105,7 @@ module.exports.index = (req, res, next) => {
       res.locals.error = { error: err.message }
       return next()
     })
+  }
 }
 
 module.exports.get = (req, res, next) => {
@@ -46,6 +138,32 @@ module.exports.store = (req, res, next) => {
       res.locals.status = 400
       return next()
     })
+}
+
+//our test endpoint functionality
+module.exports.test = (req, res, next) => {
+  let date = new Date (req.body);
+  
+  let newPost = new Post({
+    author: "Anand",
+    title: "test post",
+    text: "test text",
+    createdAt: date.toISOString()
+  });
+
+  newPost
+  .save()
+  .then(post => {
+    res.locals.data = { post }
+    res.locals.status = 200
+    return next()
+  })
+  .catch(err => {
+    console.error(err)
+    res.locals.error = { error: err.message }
+    res.locals.status = 400
+    return next()
+  })
 }
 
 module.exports.update = (req, res, next) => {
