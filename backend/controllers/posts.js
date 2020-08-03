a12cb95b825880e569bd081c71a1cbacc6483950
akrishnan93 <anand.krrishnan@gmail.com>
2020-08-03 12:39:07 -0400

    Finshed part 2

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,7 +1,99 @@
 const { Post } = require('../models')
 
 module.exports.index = (req, res, next) => {
-  Post.find()
+  const userDateRange = req.query.dateRange; 
+  let userCurrDate = new Date(req.query.currDate);
+
+  let oldDate = new Date(req.query.currDate);
+  let newDate = new Date(req.query.currDate);
+
+  if (userDateRange !== undefined) {
+    switch (userDateRange) {
+      case "Past week":
+        oldDate.setDate(userCurrDate.getDate() - 7);
+        Post.find({createdAt: {$gte: oldDate, $lte: userCurrDate }})
+        .populate('comments')
+        .sort('-createdAt')
+        .then(posts => {
+          res.locals.data = { posts }
+          res.locals.status = 200
+          return next()
+        })
+        .catch(err => {
+          console.error(err)
+          res.locals.error = { error: err.message }
+          return next()
+        })
+        break;
+      case "Past month":
+        oldDate.setDate(userCurrDate.getDate() - 31);
+        Post.find({createdAt: {$gte: oldDate, $lte: userCurrDate }})
+        .populate('comments')
+        .sort('-createdAt')
+        .then(posts => {
+          res.locals.data = { posts }
+          res.locals.status = 200
+          return next()
+        })
+        .catch(err => {
+          console.error(err)
+          res.locals.error = { error: err.message }
+          return next()
+        })
+        break;
+      case "Past year":
+        oldDate.setDate(userCurrDate.getDate() - 365);
+        Post.find({createdAt: {$gte: oldDate, $lte: userCurrDate }})
+        .populate('comments')
+        .sort('-createdAt')
+        .then(posts => {
+          res.locals.data = { posts }
+          res.locals.status = 200
+          return next()
+        })
+        .catch(err => {
+          console.error(err)
+          res.locals.error = { error: err.message }
+          return next()
+        })
+        break;
+      case "A year ago":
+        oldDate.setDate(userCurrDate.getDate() - 3650);
+        newDate.setDate(userCurrDate.getDate() - 365);
+        Post.find({createdAt: {$gte: oldDate, $lte: newDate }})
+        .populate('comments')
+        .sort('-createdAt')
+        .then(posts => {
+          res.locals.data = { posts }
+          res.locals.status = 200
+          return next()
+        })
+        .catch(err => {
+          console.error(err)
+          res.locals.error = { error: err.message }
+          return next()
+        })
+        break;
+      case "Ancient times":
+        let oldestTime = new Date(0);
+        newDate.setDate(userCurrDate.getDate() - 3650);
+        Post.find({createdAt: {$gte: oldestTime, $lte: newDate }})
+        .populate('comments')
+        .sort('-createdAt')
+        .then(posts => {
+          res.locals.data = { posts }
+          res.locals.status = 200
+          return next()
+        })
+        .catch(err => {
+          console.error(err)
+          res.locals.error = { error: err.message }
+          return next()
+        })
+        break;
+    }
+  } else {
+    Post.find()
     .populate('comments')
     .sort('-createdAt')
     .then(posts => {
@@ -14,6 +106,7 @@ module.exports.index = (req, res, next) => {
       res.locals.error = { error: err.message }
       return next()
     })
+  }
 }
 
 module.exports.get = (req, res, next) => {
